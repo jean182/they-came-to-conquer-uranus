d9dcc98e60dd1d97e250d87aa088e3195b03f544
Jean <jean-marco-10@hotmail.com>
2019-05-26 18:24:29 -0600

    Modified the template a little bit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `They Came To Conquer Uranus`,
+    description: `Let's talk about the space`,
+    author: `Jean Aguilar`,
+    siteUrl: `https://theycametoconqueruranus.com`,
+    social: {
+      twitter: `jeanm182`,
+      instagram: `jeanm182`,
+      github: `jean182`,
+      linkedIn: `jean182`,
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,10 +28,16 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#18cafc`,
+        theme_color: `#18cafc`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,50 +1,33 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { rhythm } from "../utils/typography"
 
 import Header from "./header"
-import "./layout.css"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+class Layout extends Component {
+  render() {
+    const { location, title, children } = this.props
+    console.log(location)
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <Header siteTitle={title} />
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
